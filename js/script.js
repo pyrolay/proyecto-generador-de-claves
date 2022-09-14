d94de7a54f0021aaf46c2dfbf27b57ac83770551
micaelalay <micalay003@gmail.com>
2022-09-14 02:35:37 -0300

    agrego eventos y funciones de reglas y caracteres

--- js/script.js
+++ js/script.js
@@ -6,23 +6,75 @@ const arrLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", 
 const arrNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 const arrSymbols = ["~", '"', "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "+", "=", "{", "[", "}", "]", "|", ":", ";", "'", "<", ",", ">", ".", "?", "/"]
 
+
 // VARIABLES
 const $arrInputLength = [$("#twelve-characters"), $("#nine-characters"), $("#six-characters")]
 
+const $arrInputRules = [$("#only-letters"), $("#letters-numbers"), $("#all-characters")]
+
+const $arrInputCharacters = [$("#uppercase"), $("#lowercase"), $("#numbers"), $("#symbols")]
 
 
 // FUNCTIONS
 
+let $inputLength
 const lengthFunction = () => {
     if ($("#twelve-characters").checked) $inputLength = 12
     else if ($("#nine-characters").checked) $inputLength = 9
-    else $inputLength = 6
+    else if ($("#six-characters").checked) $inputLength = 6
 }
+lengthFunction()
+
+let $inputRules
+const rulesFunction = () => {
+    if ($("#only-letters").checked) {
+        $inputRules = "only-letters"
+        $("#numbers").setAttribute("disabled", true)
+        $("#symbols").setAttribute("disabled", true)
+        $("#numbers").checked = false
+        $("#symbols").checked = false
+    } else if ($("#letters-numbers").checked) {
+        $inputRules = "letters-numbers"
+        $("#numbers").removeAttribute("disabled")
+        $("#symbols").setAttribute("disabled", true)
+        $("#symbols").checked = false
+    } else if ($("#all-characters").checked) {
+        $inputRules = "all-characters"
+        $("#numbers").removeAttribute("disabled")
+        $("#symbols").removeAttribute("disabled")
+    }
+}
+rulesFunction()
+
+let $inputCharacters
+const charactersFunction = () => {
+    let characters = []
+    for (let i = 0; i < $arrInputCharacters.length; i++) {
+        if ($arrInputCharacters[i].checked) {
+            characters.push($arrInputCharacters[i])
+        }
+    }
+    return $inputCharacters = characters
+}
+charactersFunction()
 
 // EVENTS
 
-let $inputLength
 for (const input of $arrInputLength) {
-    input.addEventListener("click", lengthFunction())
+    input.addEventListener("click", () => {
+        lengthFunction()
+    })
 }
 
+for (const input of $arrInputRules) {
+    input.addEventListener("click", () => {
+        rulesFunction()
+        charactersFunction()
+    })
+}
+
+for (const arr of $arrInputCharacters) {
+    arr.addEventListener("click", () => {
+        charactersFunction()
+    })
+}
